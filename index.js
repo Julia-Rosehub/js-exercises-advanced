52d9c8a393d95469f022cdc3b5eea2898fecdfa5
Julia Rosehub <email@example.com>
2021-03-17 13:28:55 +0300

    first commit

--- index.js
+++ index.js
@@ -1,37 +1,89 @@
 'use strict';
 
-let root = document.getElementById('root');
+let root = document.getElementById('root'),
+    timer = document.getElementById('timer');
 
-let week = ['вск', 'пнд', 'втр', 'срд', 'чтв', 'птн', 'сбт'];
-const first = week.splice(0, 1);
-const newWeek = week.push(first);
+// Первое задание
 
-const getToday = () => {
-    let today = '';
-    week.forEach((item, i) => {
-        if (i === new Date().getDay()) {
-            today = i - 1;
-        }
-    });
-    return today;
+let week = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+
+let month = ["Января", "Февраля", "Марта", "Апреля", "Мая", "Июня",
+    "Июля", "Августа", "Сентября", "Октября", "Ноября", "Декабря"
+];
+
+const HOUR_STRING = ['час', 'часа', 'часов'],
+    MINUTE_STRING = ['минута', 'минуты', 'минут'],
+    SECOND_STRING = ['секунда', 'секунды', 'секунд'];
+
+const declOfNum = (n, titles) => n + ' ' + titles[n % 10 === 1 && n % 100 !== 11 ?
+    0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
+
+let monthName = month[new Date().getMonth()];
+
+let dayName = week[new Date().getDay()],
+    dateName = new Date().getDate(),
+    yearName = new Date().getFullYear();
+
+let hours = new Date().getHours(),
+    minutes = new Date().getMinutes(),
+    seconds = new Date().getSeconds();
+
+let elem;
+
+const createNewElem = () => {
+    elem = document.createElement('div');
+    elem.textContent = `
+    Сегодня ${dayName},
+        ${dateName}
+        ${monthName}
+        ${yearName} 
+        ${declOfNum(hours, HOUR_STRING)} 
+        ${declOfNum(minutes, MINUTE_STRING)} 
+        ${declOfNum(seconds, SECOND_STRING)}
+`;
+
+    elem.style.color = 'red';
+    root.append(elem);
 };
 
-const today = getToday();
+createNewElem();
 
-week.forEach((item, i) => {
-    let newItem = document.createElement('div');
-    newItem.textContent = item;
+// Второе задание
 
-    if (item === 'сбт' || item === 'вск') {
-        newItem.style.fontStyle = 'italic';
+const prependZero = num => {
+    if (num < 10) {
+        num = '0' + num;
+    } return num;
+};
+
+const prependZeroHours = num => {
+    if (num < 12) {
+        num = '0' + num;
+    } return num;
+};
+
+elem.insertAdjacentHTML('afterend',
+    document.createElement('div').textContent = `
+            ${prependZero(dateName)}.
+            ${prependZero(new Date().getMonth())}.
+            ${yearName} - 
+            ${prependZero(hours)}:
+            ${prependZeroHours(minutes)}:
+            ${prependZeroHours(seconds)}
+`);
+
+let time = setInterval(() => {
+    while (timer.firstChild) {
+        timer.removeChild(timer.firstChild);
     }
-    if (i == today) {
-        newItem.style.fontWeight = 'bold';
-    }
 
-    root.append(newItem);
-})
-
-
-
+    timer.textContent = `
+        ${prependZero(new Date().getDate())}.
+        ${prependZero(new Date().getMonth())}.
+        ${yearName}- 
+        ${prependZero(new Date().getHours())}:
+        ${prependZeroHours(new Date().getMinutes())}:
+        ${prependZeroHours(new Date().getSeconds())}                
+  `
+}, 1000);
 
